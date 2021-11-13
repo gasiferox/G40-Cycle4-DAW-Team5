5f3fcd553dde92da126a2275ac7541f86f2a6505
Gustavo Romero <gasifero@gmail.com>
2021-11-13 03:19:12 -0500

    dev-1-> CRUD advance get and post

--- src/server.js
+++ src/server.js
@@ -13,10 +13,73 @@ mongoose.connect(connnectionString, {useNewUrlParser: true, useUnifiedTopology: 
     console.log(err)
 })
 
+// Routes
+const router = express.Router()
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+// index.html invoke
+const fs = require('fs')
+const path = require('path')
+const html_content_type = 'text/html'
+app.use(express.static('public'))
+
+router.get('/', (req, res) => {
+    res.writeHead(200, {'Content-Type': html_content_type})
+    fs.createReadStream('./src/index.html').pipe(res)
+})
+
+// Schema Imports
+const UsuarioSchema = require('./models/usuario')
+
+
 // CRUD
+router.get('/', (req, res) => {
+    res.send("API en primera versión")
+})
+
+router.get('/user', (req, res) => {
+    UsuarioSchema.find((err, data) => {
+        if(err) {
+            console.log(colors.red("Error al leer los usuarios"))
+            console.log(err)
+        }else {
+            res.send(data)
+        }
+    })
+})
+
+router.post('/user', (req, res) => {
+    let nuevoUsuario = new UsuarioSchema({
+        idUsuario: req.body.id,
+        nombreUsuario: req.body.nombre,
+        apellidoUsuario: req.body.apellido,
+        documentoUsuario: req.body.documento,
+        telefonoUsuario: req.body.telefono,
+        emailUsuario: req.body.email
+    })
+
+    nuevoUsuario.save((err, data) => {
+        if(err) {
+            console.log(colors.red("Error al grabar los datos"))
+            console.log(err)
+        }
+        res.send(colors.yellow("Usuario almacenado correctamente"))
+    })
+})
+
+/* router.put('/user/:userId', (req, res) => {
+    let usuarioEncontrado = req.body.id
+}) */
+
+router.delete('/user/:userId', (req, res) => {
+    UsuarioSchema.remove({'_userId': req.params.userId})
+    res.redirect('/user')
+})
 
 
 // Server launch
+app.use(router)
 app.listen(PORT, () => {
     console.log(colors.rainbow("Server running on port " + PORT))
 })
