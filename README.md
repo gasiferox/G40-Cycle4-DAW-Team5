a96e4cb0aa9987aaa1570adc1f874c7e86d4f1e1
Gustavo Romero <gasifero@gmail.com>
2021-11-13 00:18:25 -0500

    main-> conexion inicial mongodb con mongoose

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # G40 Cicle4 DAW Team 5
 
 ## Proyecto Inmobiliaria
+
+### Conexión a la base de datos `MongoDB`
+
+`connectionString = 'mongodb+srv://team5:<password>@clusterteam5.dix94.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'`
--- src/server.js
+++ src/server.js
@@ -2,9 +2,16 @@ const PORT = 7500
 const express = require('express')
 const app = express()
 const colors = require('colors')
+const mongoose = require('mongoose')
 
 // DB Connection
-
+const connnectionString = 'mongodb+srv://team5:team5.1234@clusterteam5.dix94.mongodb.net/inmobiliaria?retryWrites=true&w=majority'
+mongoose.connect(connnectionString, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log(colors.yellow("mongodb connected"))
+}).catch((err) => {
+    console.log(color.red("mongodb not connected"))
+    console.log(err)
+})
 
 // CRUD
 
