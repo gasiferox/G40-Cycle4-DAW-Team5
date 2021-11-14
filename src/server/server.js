9e9057d878e0287f054f7a48571f874193472b27
MARIA PATRICIA GUEVARA <mariapatriciaguevaracastano@gmail.com>
2021-11-14 01:54:25 -0500

    cambio color

--- src/server/server.js
+++ src/server/server.js
@@ -13,9 +13,9 @@ const userRoutes = require('./routes/user')
 // DB Connection
 const connnectionString = 'mongodb+srv://team5:team5.1234@clusterteam5.dix94.mongodb.net/inmobiliaria?retryWrites=true&w=majority'
 mongoose.connect(connnectionString, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
-    console.log(colors.yellow("mongodb connected"))
+    console.log(colors.bgGreen("mongodb connected"))
 }).catch((err) => {
-    console.log(color.red("mongodb not connected"))
+    console.log(color.bgred("mongodb not connected"))
     console.log(err)
 })
 
