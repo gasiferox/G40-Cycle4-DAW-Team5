50dd6251e5fabfb83ad4908ddfdf5e2894bead6c
Gustavo Romero <gasifero@gmail.com>
2021-11-18 01:59:59 -0500

    dev1front-> shared components were created (header, footer and sidebar)

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SideBarComponent
+  ],
   imports: [
     CommonModule
   ]
