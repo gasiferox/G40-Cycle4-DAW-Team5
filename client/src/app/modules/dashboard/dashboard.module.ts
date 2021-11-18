43144eacdcf3a1cba1daed273133fa9f95ef9c69
Gustavo Romero <gasifero@gmail.com>
2021-11-18 03:04:56 -0500

    dev1front-> side-bar component included into home-page component

--- client/src/app/modules/dashboard/dashboard.module.ts
+++ client/src/app/modules/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    SharedModule
   ]
 })
 export class DashboardModule { }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -14,6 +14,9 @@ import { SideBarComponent } from './components/side-bar/side-bar.component';
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    SideBarComponent
   ]
 })
 export class SharedModule { }
