3f18a5160adecaa830c1b407fbaff9a633e73269
Gustavo Romero <gasifero@gmail.com>
2021-11-18 01:25:33 -0500

    dev1front-> first fourth modules created

---
+++ client/src/app/modules/properties/properties.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PropertiesRoutingModule } from './properties-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PropertiesRoutingModule
+  ]
+})
+export class PropertiesModule { }
