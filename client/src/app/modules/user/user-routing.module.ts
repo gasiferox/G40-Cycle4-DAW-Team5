56baedb7977bfeb3528d3d3a7397434f24ba1cd5
MARIA PATRICIA GUEVARA <mariapatriciaguevaracastano@gmail.com>
2021-11-24 14:43:52 -0500

    cambios

--- client/src/app/modules/user/user-routing.module.ts
+++ client/src/app/modules/user/user-routing.module.ts
@@ -4,6 +4,18 @@ import { UsuarioComponent } from './usuario/usuario.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AcercaComponent } from './acerca/acerca.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { AddUserComponent } from '@modules/admin/users/add-user/add-user.component';
+import { EditUserComponent } from '@modules/admin/users/edit-user/edit-user.component';
+import { DeleteUserComponent } from '@modules/admin/users/delete-user/delete-user.component';
+import { GetAllUsersComponent } from '@modules/admin/users/get-all-users/get-all-users.component';
+import { AddLocationComponent } from '@modules/admin/locations/add-location/add-location.component';
+import { GetAllLocationsComponent } from '@modules/admin/locations/get-all-locations/get-all-locations.component';
+import { EditLocationComponent } from '@modules/admin/locations/edit-location/edit-location.component';
+import { DeleteLocationComponent } from '@modules/admin/locations/delete-location/delete-location.component';
+import { AddPropertyComponent } from '@modules/admin/properties/add-property/add-property.component';
+import { GetAllPropertiesComponent } from '@modules/admin/properties/get-all-properties/get-all-properties.component';
+import { EditPropertyComponent } from '@modules/admin/properties/edit-property/edit-property.component';
+import { DeletePropertyComponent } from '@modules/admin/properties/delete-property/delete-property.component';
 
 
 const routes: Routes = [
@@ -27,6 +39,54 @@ component: DashboardComponent
 {
 path: 'ContactoComponent',
 component:  ContactoComponent
+},
+{
+  path: 'add-user',
+  component: AddUserComponent
+},
+{
+  path: 'get-all-users',
+  component: GetAllUsersComponent
+},
+{
+  path: 'edit-user',
+  component: EditUserComponent
+},
+{
+  path: 'delete-user',
+  component: DeleteUserComponent
+},
+{
+  path: 'add-location',
+  component: AddLocationComponent
+},
+{
+  path: 'get-all-locations',
+  component: GetAllLocationsComponent
+},
+{
+  path: 'edit-location',
+  component: EditLocationComponent
+},
+{
+  path: 'delete-location',
+  component: DeleteLocationComponent
+},
+{
+  path: 'add-property',
+  component: AddPropertyComponent
+},
+{
+  path: 'get-all-properties',
+  component: GetAllPropertiesComponent
+},
+{
+  path: 'edit-property',
+  component: EditPropertyComponent
+},
+{
+  path: 'delete-property',
+  component: DeletePropertyComponent
 }
 ];
 
--- client/src/app/shared/side-bar-home/side-bar-home.component.ts
+++ client/src/app/shared/side-bar-home/side-bar-home.component.ts
@@ -25,99 +25,21 @@ export class SideBarHomeComponent implements OnInit {
 
     this.mainMenu.userOptions = [
       {
-        name: 'Agregar Usuario',
+        name: 'Home',
         icon: 'uil-plus',
-        router: ['/', 'admin', 'add-user']
-      },
-      /* {
-        name: 'Listar Usuario por Id',
-        icon: 'uil-list-ol',
-        router: ['/', 'admin', 'get-user-by-id']
-      }, */
-      {
-        name: 'Listar Usuarios',
-        icon: 'uil-list-ol',
-        router: ['/', 'admin', 'get-all-users']
+        router: ['/', 'user', 'dashboard']
       },
       {
-        name: 'Actualizar usuario',
+        name: 'Acerca de',
+        icon: 'uil-list-ol',
+        router: ['/', 'user', 'acerca']
+      },
+      {
+        name: 'Contacto',
         icon: 'uil-edit',
-        router: ['/', 'admin', 'edit-user']
+        router: ['/', 'user', 'contacto']
       },
-      {
-        name: 'Borrar usuario',
-        icon: 'uil-trash-alt',
-        router: ['/', 'admin', 'delete-user']
-      }
+      
     ]
-
-    this.mainMenu.locationOptions = [
-      {
-        name: 'Agregar Ubicación',
-        icon: 'uil-plus',
-        router: ['/', 'admin', 'add-location']
-      },
-      /* {
-        name: 'Listar Ubicación por Id',
-        icon: 'uil-list-ol',
-        router: ['/', 'admin', 'get-location-by-id']
-      }, */
-      {
-        name: 'Listar ubicaciones',
-        icon: 'uil-list-ol',
-        router: ['/', 'admin', 'get-all-locations']
-      },
-      {
-        name: 'Actualizar Ubicación',
-        icon: 'uil-edit',
-        router: ['/', 'admin', 'edit-location']
-      },
-      {
-        name: 'Borrar Ubicación',
-        icon: 'uil-trash-alt',
-        router: ['/', 'admin', 'delete-location']
-      }
-    ]
-
-    this.mainMenu.propertyOptions = [
-      {
-        name: 'Agregar Inmueble',
-        icon: 'uil-plus',
-        router: ['/', 'admin', 'add-property']
-      },
-      /* {
-        name: 'Listar Inmueble por Id',
-        icon: 'uil-list-ol',
-        router: ['/', 'admin', 'get-property-by-id']
-      }, */
-      {
-        name: 'Listar Inmuebles',
-        icon: 'uil-list-ol',
-        router: ['/', 'admin', 'get-all-properties']
-      },
-      {
-        name: 'Actualizar Inmueble',
-        icon: 'uil-edit',
-        router: ['/', 'admin', 'edit-property']
-      },
-      {
-        name: 'Borrar Inmueble',
-        icon: 'uil-trash-alt',
-        router: ['/', 'admin', 'delete-property']
-      }
-    ]
-
   }
-
-  /* goTo($event: any): void {
-    this.router.navigate(['/', 'admin'], {
-      queryParams: {
-        key1: 'value1',
-        key2: 'value2',
-        key3: 'value3'
-      }
-    })
-    console.log($event)
-  } */
-
 }
