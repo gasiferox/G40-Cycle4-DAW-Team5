c6f9460e75d98844230c8954951085b9916818c9
Leonardo Jiménez <ljim230@gmail.com>
2021-11-30 23:37:38 -0500

    dev 5 -> Path dashboard, CRUD and Error pages

--- client/src/app/modules/home/home-routing.module.ts
+++ client/src/app/modules/home/home-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from '@modules/user/dashboard/dashboard.component';
+import { ErrorComponent } from '@shared/error/error.component';
 
 const routes: Routes = [
   {
     path: 'admin',
     loadChildren: () => import('@modules/admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: "",
+    component: DashboardComponent
+  },
+  {
+    path: "**",
+    component: ErrorComponent
   }
 ];
 
