c9def5999e6fdd8eda7d3187410d88a37a5ef15f
Leonardo Jiménez <ljim230@gmail.com>
2021-12-02 00:25:57 -0500

    dev5 -> CRUD Properties - Frontend

--- client/src/app/modules/admin/properties/get-all-properties/get-all-properties.component.ts
+++ client/src/app/modules/admin/properties/get-all-properties/get-all-properties.component.ts
@@ -7,9 +7,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GetAllPropertiesComponent implements OnInit {
 
+  mockDb: {
+    mockUsers: Array<any>, mockLocations: Array<any>, mockProperties: Array<any>
+  } = { mockUsers: [], mockLocations: [], mockProperties: [] }
+
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.mockDb.mockProperties = [
+      {
+        id: '1',
+        tipo: "Apartamento",
+        ubicacion: "",
+        area: '94',
+        precio: '685000000',
+        habitaciones: '2',
+        banos: '2',
+        parqueadero: 'false',
+        descripcion: "Apartamento amoblado",
+        imagenes: ""
+      },
+      {
+        id: '2',
+        tipo: "Apartamento",
+        ubicacion: "",
+        area: '65.5',
+        precio: '150000000',
+        habitaciones: '3',
+        banos: '2',
+        parqueadero: 'true',
+        descripcion: "Recien remodelado, pisos en ceramica y cocina integral",
+        imagenes: ""
+      },
+      {
+
+        id: '3',
+        tipo: "casa",
+        ubicacion: "",
+        area: '380',
+        precio: '850000000',
+        habitaciones: '4',
+        banos: '4',
+        parqueadero: 'true',
+        descripcion: "Casa campestre",
+        imagenes: ""
+      },
+      {
+        id: '4',
+        tipo: "casa",
+        ubicacion: "",
+        area: '90',
+        precio: '120000000',
+        habitaciones: '3',
+        banos: '2',
+        parqueadero: 'true',
+        descripcion: "Casa residencial",
+        imagenes: ""
+      },
+      {
+        id: '5',
+        tipo: "apartamento",
+        ubicacion: "",
+        area: '72',
+        precio: '120000000',
+        habitaciones: '2',
+        banos: '1',
+        parqueadero: 'false',
+        descripcion: "Apartamento residencial tercer piso",
+        imagenes: ""
+      },
+      {
+        id: '6',
+        tipo: "casa",
+        ubicacion: "",
+        area: '100',
+        precio: '250000000',
+        habitaciones: '2',
+        banos: '2',
+        parqueadero: 'true',
+        descripcion: "Casa en conjunto cerrado",
+        imagenes: ""
+      }
+    ]
   }
 
 }
