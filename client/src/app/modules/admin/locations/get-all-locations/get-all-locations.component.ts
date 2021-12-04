04264a3a5b7f0bdd4eafd49f33b92b04f76f3fbe
Leonardo Jiménez <ljim230@gmail.com>
2021-12-03 22:52:46 -0500

    dev5 -> CRUD Locations Frontend

--- client/src/app/modules/admin/locations/get-all-locations/get-all-locations.component.ts
+++ client/src/app/modules/admin/locations/get-all-locations/get-all-locations.component.ts
@@ -7,9 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GetAllLocationsComponent implements OnInit {
 
+  mockDb: {
+    mockUsers: Array<any>, mockLocations: Array<any>, mockProperties: Array<any>
+  } = { mockUsers: [], mockLocations: [], mockProperties: [] }
+
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.mockDb.mockLocations = [
+      {
+        id: '1',
+        departamento: "Cundinamarca",
+        ciudad: "Bogotá",
+        zona: 'Norte',
+        vereda: ''
+      },
+      {
+        id: '2',
+        departamento: "Risaralda",
+        ciudad: "Pereira",
+        zona: 'Este',
+        vereda: 'Tribunas2'
+      },
+      {
+        id: '3',
+        departamento: "Caquetá",
+        ciudad: "Florencia",
+        zona: 'Occidente',
+        vereda: ''
+      },
+      {
+        id: '4',
+        departamento: "Qundío",
+        ciudad: "Armenia",
+        zona: 'Centro',
+        vereda: '',
+      },
+      {
+        id: '5',
+        departamento: "Risaralda",
+        ciudad: "Pereira",
+        zona: 'Este',
+        vereda: 'Tribunas',
+      }
+    ]
   }
 
 }
